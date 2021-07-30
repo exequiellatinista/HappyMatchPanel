b23aee860587a7f1f185cf9c0a944fcc2affcb92
exel3 <exequiellatinista@gmail.com>
2021-07-30 00:04:38 -0300

    feat: add ColorSwitch component

---
+++ components/ui/ColorSwitch.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      
+        <p>HOLA LINTER</p>
+          <IconDark />
+    </div>
+</template>
+<script>
+export default {
+    name: 'ColorSwitch',
+    components: {
+        IconDark: () => import('@/assets/icons/dark.svg?inline')
+    }
+}
+</script>
