06f2e01a6927351133566226ea9547c30ba0b76d
exequiel <exequiellatinista@gmail.com>
2021-09-01 13:37:15 -0300

    feat: test vercel

--- api/index.js
+++ api/index.js
@@ -51,3 +51,12 @@ app.post('/', (req, res) => {
 })
 
 module.exports = app
+
+// Start standalone server if directly running
+if (require.main === module) {
+  const port = process.env.PORT || 3001
+  app.listen(port, () => {
+    // eslint-disable-next-line no-console
+    console.log(`API server listening on port ${port}`)
+  })
+}
