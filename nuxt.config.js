99f2aeccfb7b8f3e8d9e1c0f989e22b0e1ce566f
exel3 <exequiellatinista@gmail.com>
2021-09-28 18:26:31 -0300

    feat: delete color mode

--- nuxt.config.js
+++ nuxt.config.js
@@ -63,16 +63,6 @@ export default {
     }
   },
 
-  colorMode: {
-    preference: 'system', // default value of $colorMode.preference
-    fallback: 'light', // fallback value if not system preference found
-    hid: 'nuxt-color-mode-script',
-    globalName: '__NUXT_COLOR_MODE__',
-    componentName: 'ColorScheme',
-    classPrefix: '',
-    classSuffix: '-mode',
-    storageKey: 'nuxt-color-mode'
-  },
 
   firebase: {
     config: {
